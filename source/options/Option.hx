4833689eac96221fa68780d250b1eedfa8ba2d95
Prohack101010 <126958051+Prohack101010@users.noreply.github.com>
2024-02-12 23:07:38 +0300

    Update Option.hx

--- source/options/Option.hx
+++ source/options/Option.hx
@@ -21,6 +21,7 @@ import flixel.input.keyboard.FlxKey;
 import flixel.graphics.FlxGraphic;
 import Controls;
 
+
 using StringTools;
 
 class Option
@@ -34,6 +35,7 @@ class Option
 	// Everything else will use a text
 
 	public var showBoyfriend:Bool = false;
+	public var showNote:Bool = false;
 	public var scrollSpeed:Float = 50; //Only works on int/float, defines how fast it scrolls per second while holding left/right
 
 	private var variable:String = null; //Variable from ClientPrefs.hx
