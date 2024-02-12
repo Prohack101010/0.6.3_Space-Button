6829ec7ada321c06f6995999b879b4aa88cfdc25
Prohack101010 <126958051+Prohack101010@users.noreply.github.com>
2024-02-12 22:53:55 +0300

    Update FlxHitbox.hx

--- source/android/FlxHitbox.hx
+++ source/android/FlxHitbox.hx
@@ -3,76 +3,59 @@ package android;
 import flixel.graphics.frames.FlxAtlasFrames;
 import flixel.graphics.FlxGraphic;
 import flixel.group.FlxSpriteGroup;
+import flixel.tweens.FlxTween;
+import flixel.tweens.FlxEase;
 import flixel.ui.FlxButton;
 import flixel.FlxSprite;
 
 class FlxHitbox extends FlxSpriteGroup {
 	public var hitbox:FlxSpriteGroup;
-	public var hint:FlxSpriteGroup;
+
 	public var buttonLeft:FlxButton;
 	public var buttonDown:FlxButton;
 	public var buttonUp:FlxButton;
 	public var buttonRight:FlxButton;
-	public var buttonHintLeft:FlxSprite;
-	public var buttonHintDown:FlxSprite;
-	public var buttonHintUp:FlxSprite;
-	public var buttonHintRight:FlxSprite;
 
-	public function new() {
+	public var orgAlpha:Float = 0.75;
+	public var orgAntialiasing:Bool = true;
+	
+	public function new(?alphaAlt:Float = 0.75, ?antialiasingAlt:Bool = true) {
 		super();
 
-		hitbox = new FlxSpriteGroup();
-		hint = new FlxSpriteGroup();
+		orgAlpha = alphaAlt;
+		orgAntialiasing = antialiasingAlt;
 
 		buttonLeft = new FlxButton(0, 0);
 		buttonDown = new FlxButton(0, 0);
 		buttonUp = new FlxButton(0, 0);
 		buttonRight = new FlxButton(0, 0);
 
-        buttonHintLeft = new FlxSprite(0, 0);
-        buttonHintDown = new FlxSprite(0, 0);
-        buttonHintUp = new FlxSprite(0, 0);
-        buttonHintRight = new FlxSprite(0, 0);
+		hitbox = new FlxSpriteGroup();
+		hitbox.add(add(buttonLeft = createhitbox(0, 0, "left")));
+		hitbox.add(add(buttonDown = createhitbox(320, 0, "down")));
+		hitbox.add(add(buttonUp = createhitbox(640, 0, "up")));
+		hitbox.add(add(buttonRight = createhitbox(960, 0, "right")));
 
-		hitbox.add(add(buttonLeft = createHitbox(0, 0, 0xC457D3)));
-		hitbox.add(add(buttonDown = createHitbox(320, 0, 0x00DAFF)));
-		hitbox.add(add(buttonUp = createHitbox(640, 0, 0x00FF00)));
-		hitbox.add(add(buttonRight = createHitbox(960, 0, 0xFF0000)));
-
-		hint.add(add(buttonHintLeft = createHitboxHint(0, 0, 0xC457D3)));
-		hint.add(add(buttonHintDown = createHitboxHint(320, 0, 0x00DAFF)));
-		hint.add(add(buttonHintUp = createHitboxHint(640, 0, 0x00FF00)));
-		hint.add(add(buttonHintRight = createHitboxHint(960, 0, 0xFF0000)));
+		var hitbox_hint:FlxSprite = new FlxSprite(0, 0).loadGraphic(Paths.image('androidcontrols/hitbox_hint'));
+		hitbox_hint.antialiasing = orgAntialiasing;
+		hitbox_hint.alpha = orgAlpha;
+		add(hitbox_hint);
 	}
 
-    public function createHitbox(x:Float = 0, y:Float = 0, colors:Int = 0xFFFFFF) {
-        var button:FlxButton = new FlxButton(x,y);
-        button.loadGraphic(Paths.image('androidcontrols/hitbox'));
-        button.updateHitbox();
-        button.color = colors;
-        button.alpha = 0;
-        add(button);
+	public function createhitbox(x:Float = 0, y:Float = 0, frames:String) {
+		var button = new FlxButton(x, y);
+		button.loadGraphic(FlxGraphic.fromFrame(getFrames().getByName(frames)));
+		button.antialiasing = orgAntialiasing;
+		button.alpha = 0;// sorry but I can't hard lock the hitbox alpha
+		button.onDown.callback = function (){FlxTween.num(0, 0.75, 0.075, {ease:FlxEase.circInOut}, function(alpha:Float){ button.alpha = alpha;});};
+		button.onUp.callback = function (){FlxTween.num(0.75, 0, 0.1, {ease:FlxEase.circInOut}, function(alpha:Float){ button.alpha = alpha;});}
+		button.onOut.callback = function (){FlxTween.num(button.alpha, 0, 0.2, {ease:FlxEase.circInOut}, function(alpha:Float){ button.alpha = alpha;});}
+		return button;
+	}
 
-        button.onOut.callback = function() {button.alpha = 0;}
-        button.onDown.callback = function() {button.alpha = ClientPrefs.mobileControlOpacity;}
-        button.onUp.callback = function() {button.alpha = 0;}
-
-        return button;
-    }
-
-    public function createHitboxHint(x:Float = 0, y:Float = 0, colors:Int = 0xFFFFFF) {
-        var buttonHint:FlxSprite = new FlxSprite(x,y);
-        buttonHint.loadGraphic(Paths.image('androidcontrols/hint'));
-		if (ClientPrefs.hideHint) {
-            buttonHint.alpha = 0;
-		} else {
-            buttonHint.alpha = ClientPrefs.mobileControlOpacity;
-		}
-		buttonHint.color = colors;
-        add(buttonHint);
-
-        return buttonHint;
-    }
+	public function getFrames():FlxAtlasFrames {
+		return Paths.getSparrowAtlas('androidcontrols/hitbox');
+	}
 
 	override public function destroy():Void {
 		super.destroy();
